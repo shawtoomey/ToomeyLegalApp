be1f10a86e7e6fdb77de73bcf483be0fea167a57
shawtoomey <85414367+shawtoomey@users.noreply.github.com>
2022-11-02 10:55:00 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React from 'react';
 import MatterInfo from './MatterInfo';
 import Quotation from './Quotation';
+import ReactDOM from "react-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -18,19 +19,18 @@ import {
 function App() {
   const isBackgroundPurple = true;
   return (
-   <div className="App">
+    <div className="App">
     <Navbar collapseOnSelect expand="lg" variant="dark" fixed="top">
         <Container>
-          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <h1>Toomey Legal </h1>
           <Nav className="me-auto">
-            <Nav.Link href="#quotation">Quotation</Nav.Link>
-            <Nav.Link href="#matters">Matters</Nav.Link>
-            <Nav.Link href="#login">Login</Nav.Link>
-            <Nav.Link href="#sign-up">Sign Up</Nav.Link>
+            <li href="#quotation">Quotation | </li>
+            <li href="#matters">Matters | </li>
+            <li href="#login">Login | </li>
+            <li href="#sign-up">Sign Up</li>
           </Nav>
         </Container>
       </Navbar>
-      <br />
     <header className="App-header">
       <a>
       <img className="logo" src="https://scontent.fbrs4-1.fna.fbcdn.net/v/t39.30808-6/273536999_3005458816373113_1211053597501658920_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=Q5d9DWS0qLYAX8c8Qgt&_nc_ht=scontent.fbrs4-1.fna&oh=00_AT8ibaMLNisd5f8in-lzJ0YUArDPLdoNcL4NMtFZMiPnQQ&oe=6356667E"></img>
@@ -49,8 +49,7 @@ function App() {
         <button type="submit">Receive a Quote</button>
       </form>  
     </header>
-  </div>
-
+    </div>
   );
 }
 
--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -17,8 +17,18 @@ import {
 
   function CreateAccount() {
     return (
-        <div className = "AccountCreation">
-            const isBackgroundPurple = true;
+        <div className="App">
+    <Navbar collapseOnSelect expand="lg" variant="dark" fixed="top">
+        <Container>
+          <h1>Toomey Legal </h1>
+          <Nav className="me-auto">
+            <li href="#quotation">Quotation | </li>
+            <li href="#matters">Matters | </li>
+            <li href="#login">Login | </li>
+            <li href="#sign-up">Sign Up</li>
+          </Nav>
+        </Container>
+      </Navbar>
             <a>
               <img src="https://scontent.fbrs4-1.fna.fbcdn.net/v/t39.30808-6/273536999_3005458816373113_1211053597501658920_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=Q5d9DWS0qLYAX8c8Qgt&_nc_ht=scontent.fbrs4-1.fna&oh=00_AT8ibaMLNisd5f8in-lzJ0YUArDPLdoNcL4NMtFZMiPnQQ&oe=6356667E">
               </img>
